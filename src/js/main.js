3cbc18017a5d9813d4d78425a366b4d4a2cbd39a
MD. Azharul Islam <113032991+sazharul@users.noreply.github.com>
2024-10-27 04:29:50 +0600

    update responsive

--- src/js/main.js
+++ src/js/main.js
@@ -25,3 +25,15 @@ var wow = new WOW(
     }
 );
 wow.init();
+
+window.toggleMenu = function (x) {
+    x.classList.toggle("change");
+    // Toggle the hamburger animation
+
+    // Toggle the mobile menu visibility
+    const menuSection = document.querySelector('.mobile_menu_section');
+    menuSection.classList.toggle('menu-open');
+
+    const body = document.querySelector('body');
+    body.classList.toggle('no-scroll');
+}
